8a7edb1addb6b6f985ccb556eb03c623635f3769
Foster Stule <camodude5@yahoo.com>
2018-06-21 09:21:46 -0400

    intial commit

---
+++ MVCAssignment1/Models/Owner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAssignment1.Models
+{
+	public class Owner
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string State { get; set; }
+		public string City { get; set; }
+
+		public Owner() { }
+	}
+}
